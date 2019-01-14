66455e6703c0fb95c399aa3a78b0f85fa963304b
Shawn Hooper <shooper@COV-SHOOPER-MBP.local>
2019-01-14 15:55:59 -0500

    Update knife.rb and add .gitignore

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,12 @@
 # Amazon AWS
 knife[:aws_credential_file] = "/home/centos/aws.txt"
 
-#cookbook_path ["#{current_dir}/../cookbooks"]
-cookbook_path [ '.', '..', './cookbooks', '~/chef-repo' ]
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+chef_server_url          "https://ip-172-31-47-233.ec2.internal/organizations/coveros"
+cookbook_path            ["#{current_dir}/../cookbooks"]
